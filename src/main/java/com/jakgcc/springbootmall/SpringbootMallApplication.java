054424a3d4540ff73abe9e573f4860b226a274c6
chanpinlai <jakgcc@gmail.com>
2022-12-19 01:49:40 +0800

    debug

--- src/main/java/com/jakgcc/springbootmall/SpringbootMallApplication.java
+++ src/main/java/com/jakgcc/springbootmall/SpringbootMallApplication.java
@@ -2,12 +2,22 @@ package com.jakgcc.springbootmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootMallApplication {
+//public class SpringbootMallApplication {
+public class SpringbootMallApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootMallApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootMallApplication.class);
+    }
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMallApplication.class, args);
+    }
 
 }
