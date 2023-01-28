49aed8c187c1272569ae0293b1083af96801f808
chanpinlai <jakgcc@gmail.com>
2023-01-28 22:16:17 +0800

    改port為80

--- src/main/java/com/jakgcc/springbootmall/test/Test.java
+++ src/main/java/com/jakgcc/springbootmall/test/Test.java
@@ -1,8 +1,17 @@
 package com.jakgcc.springbootmall.test;
 
+import java.util.ArrayList;
+
 public class Test {
     public static void main(String[] args) {
         int[] nums = { -1, 3, -10, -20 };
         System.out.println(nums[2]*nums[3]);
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("d");
+        arrayList.add("a");
+        arrayList.add("c");
+        arrayList.add("b");
+        arrayList.set(1,"f");
+        System.out.println(arrayList.toString());
     }
 }
